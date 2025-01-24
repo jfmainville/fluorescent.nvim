4708e1b312f7ea560481f67d29b8258fb3d26c2f
Jean-Frederic Mainville <jfmainville@outlook.com>
2025-01-23 21:07:54 -0500

    refactor: add the fg attribute to the RenderMarkdownH*Bg highlights for the render-markdown.nvim plugin

--- lua/fluorescent/highlights/fluorescent/rendermarkdown.lua
+++ lua/fluorescent/highlights/fluorescent/rendermarkdown.lua
@@ -4,12 +4,12 @@ local M = {}
 M.load = function(opts)
   local hl = opts.utils.set_hl
 
-  hl("RenderMarkdownH1Bg", { bg = "#242739" })
-  hl("RenderMarkdownH2Bg", { bg = "#282C28" })
-  hl("RenderMarkdownH3Bg", { bg = "#262C2C" })
-  hl("RenderMarkdownH4Bg", { bg = "#1C2930" })
-  hl("RenderMarkdownH5Bg", { bg = "#292739" })
-  hl("RenderMarkdownH6Bg", { bg = "#262335" })
+  hl("RenderMarkdownH1Bg", { fg = "#86A2E8", bg = "#242739" })
+  hl("RenderMarkdownH2Bg", { fg = "#D3AA6C", bg = "#282C28" })
+  hl("RenderMarkdownH3Bg", { fg = "#ACCA87", bg = "#262C2C" })
+  hl("RenderMarkdownH4Bg", { fg = "#58B59C", bg = "#1C2930" })
+  hl("RenderMarkdownH5Bg", { fg = "#B6A0EB", bg = "#292739" })
+  hl("RenderMarkdownH6Bg", { fg = "#9780CC", bg = "#262335" })
   hl("RenderMarkdownH1", { fg = "#86A2E8" })
   hl("RenderMarkdownH2", { fg = "#D3AA6C" })
   hl("RenderMarkdownH3", { fg = "#ACCA87" })
